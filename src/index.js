c684b7d0a7a13dfb214d6576da91d75382071def
PaFkaTa133 <119074272+PaFkaTa133@users.noreply.github.com>
2022-12-02 14:51:28 +0200

    Fix: Fetching data from api

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import "./scss/app.scss";
 import Application from "./js/Application";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-  const app = new Application();
-  // Used to access the app instance by the automated tests
-  window.__JS_APP = app;
+    // This block will be executed once the page is loaded and ready
+    const app = new Application();
+    // Used to access the app instance by the automated tests
+    window.__JS_APP = app;
+});
+
+//fetching planet api
+
+let url = 'https://swapi.boom.dev/api/planets';
+fetch(url).then((response) => {
+    if (response.status !== 200) {
+        console.log(`Error: ${response.status}`);
+        return;
+    }
+
+    response.json().then((data) => {
+        console.log(data);
+    });
+
 });
